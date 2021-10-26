2ea4bae69d272257ee75dfa0cc55975e50d86d3b
amargapinho <91600363+amargapinho@users.noreply.github.com>
2021-10-26 17:54:27 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+
+namespace Zufallsgenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            int N = 5;
+            int b = 7;
+            int m = 9;
+
+            int seed = 78;
+
+            List<int> a = new List<int>();
+            a.Add(seed);
+
+            for(int i = 1; i < N; i++)
+            {
+                a.Add((a.ElementAt(i - 1) * b + 1) % m);
+                Console.WriteLine(a.ElementAt(i));
+            }
+
+            Console.WriteLine(seed);
+
+
+   
+        }
+    }
+}
